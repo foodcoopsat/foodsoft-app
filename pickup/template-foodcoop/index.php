0d447b29fe2ecf2daddbea1e3a86af9ee1d8ee66
mjavurek <57668028+mjavurek@users.noreply.github.com>
2026-02-14 15:09:45 +0100

    Add index and initial configuration for foodcoop template

---
+++ pickup/template-foodcoop/index.php
@@ -0,0 +1,40 @@
+<?php
+
+$config = [
+    // "foodcoop_name" => "...", // default: ucwords(current url dir)
+    // "foodsoft_url" => "https://app.foodcoops.at/...", // default: "https://app.foodcoops.at/" + current url dir
+
+    // Einrichten:  https://app.foodcoops.at/.../oauth/applications
+    // Callback URL: https://pickup.foodcoops.at/.../
+    // Client (application) credentials von dort: 
+    "client_id" => "...",
+    "client_secret" => "...",
+
+    "exclude_usernames" => [
+        "Bestellungen",
+        "admin"
+    ], // default: []
+];
+
+
+// ----------------------------------------------------------
+// development: test settings for local foodsoft
+// ----------------------------------------------------------
+
+$config["runs_on_local_server"] = $_SERVER["HTTP_HOST"] == "localhost";
+$config["use_local_foodsoft"] = $config["runs_on_local_server"];
+$config["debug"] = FALSE; #$runs_on_local_server;  #TRUE; #FALSE;
+
+if ($config["use_local_foodsoft"]) {
+    $config["foodsoft_url"] = "http://localhost:3000/f";
+    // Einrichten:  localhost:3000/f/oauth/applications
+    // Callback URL: http://localhost/...
+    $config["time_now"] = "2025-12-17"; // "today"; date should be the date of the last local foodsoft database update
+}
+// --- end of development section ---------------------------------------
+
+
+
+include "../main.php";
+
+?>
